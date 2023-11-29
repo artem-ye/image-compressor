68c636538ad35b91c4564ebe8755f7708aa9604a
ay <shelmafona@gmail.com>
2023-11-29 21:46:48 +0200

    refactor

--- src/getImage.js
+++ src/getImage.js
@@ -1,10 +1,6 @@
 import sharp from 'sharp';
 import http from 'http';
 
-// const get = async (url) => {
-// 	return (await axios.get(url, { responseType: 'arraybuffer' })).data;
-// };
-
 const get = (url) => {
 	return new Promise((resolve, reject) => {
 		http.get(url, (res) => {
