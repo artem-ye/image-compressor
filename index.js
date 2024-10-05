8784f7edddb37f08eac26c5b1aea5574a74c97fc
ay <shelmafona@gmail.com>
2024-10-05 20:34:54 +0300

    decompose: move helpers to separated file

--- index.js
+++ index.js
@@ -5,31 +5,23 @@ import sharp from 'sharp';
 import { config } from './lib/config.js';
 import { logger } from './lib/logger.js';
 import { parseUrl, parseResizeOpts } from './lib/url.js';
+import { upstreamUrl, buildResponseHeaders } from './lib/helpers.js';
 
-const { host: HOST, port: PORT, upstreamEndpoint: UPSTREAM_ENDPOINT } = config;
+const { host: HOST, port: PORT } = config;
 const HTTP_INTERNAL_ERROR = 500;
 const HTTP_OK = 200;
 
-const upstreamUrl = (path) => `${UPSTREAM_ENDPOINT}${path}`;
-
-const buildResponseHeaders = ({ headers: requestHeaders }) => {
-  const headers = { 'transfer-encoding': 'chunked' };
-  const { 'content-type': contentType } = requestHeaders;
-  contentType && (headers['content-type'] = contentType);
-  return headers;
-};
-
-const stub = (err) => void (err && logger.error(err.message));
+// prettier-ignore
+const errorLogger = ({ method, url }) => (msg) =>
+  logger.child({ method }).child({ url }).error(msg);
 
 const server = http.createServer((request, response) => {
   const { path, params } = parseUrl(request.url);
   const imageUrl = upstreamUrl(path);
   const resizeOpts = parseResizeOpts(params);
 
-  const logError = (msg) => {
-    const { method, url } = request;
-    logger.child({ method }).child({ url }).error(msg);
-  };
+  const logError = errorLogger(request);
+  const pipelineStub = (err) => void (err && logError(err.message));
 
   const abort = (statusCode, statusMessage) => {
     response.headersSent || response.writeHead(statusCode, statusMessage);
@@ -43,19 +35,17 @@ const server = http.createServer((request, response) => {
       logError(`Upstream bad response: ${statusCode} ${statusMessage}`);
       abort(statusCode, statusMessage);
     } else {
-      const resize = sharp()
-        .resize(resizeOpts)
-        .once('readable', () => {
-          const headers = buildResponseHeaders(upstreamResponse);
-          response.writeHead(statusCode, statusMessage, headers);
-        })
-        .on('error', (err) => {
-          logError(`Resize error: ${err.message}`);
-          abort(HTTP_INTERNAL_ERROR, 'Resize error');
-          resize.destroy();
-        });
-
-      pipeline(upstreamResponse, resize, response, stub);
+      const resize = sharp().resize(resizeOpts);
+      resize.once('readable', () => {
+        const headers = buildResponseHeaders(upstreamResponse);
+        response.writeHead(statusCode, statusMessage, headers);
+      });
+      resize.on('error', (err) => {
+        logError(`Resize error: ${err.message}`);
+        abort(HTTP_INTERNAL_ERROR, 'Resize error');
+        resize.destroy();
+      });
+      pipeline(upstreamResponse, resize, response, pipelineStub);
     }
   });
 
@@ -64,9 +54,10 @@ const server = http.createServer((request, response) => {
     abort(HTTP_INTERNAL_ERROR, 'Upstream communication error');
   });
 
-  pipeline(request, upstreamReq, stub);
+  pipeline(request, upstreamReq, pipelineStub);
 });
 
 server.listen(PORT, HOST, () => {
-  logger.info('Server started on', JSON.stringify(server.address()));
+  const { address, port } = server.address();
+  logger.info(`Server started on ${address}:${port}`);
 });
