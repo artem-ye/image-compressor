b16047e9f229c4b471084d8dfd859e396fb7f27d
ay <shelmafona@gmail.com>
2024-10-08 21:42:24 +0300

    v1.6.0

--- index.js
+++ index.js
@@ -4,59 +4,52 @@ import sharp from 'sharp';
 
 import { config } from './lib/config.js';
 import { logger } from './lib/logger.js';
-import { parseUrl, parseResizeOpts } from './lib/url.js';
-import { upstreamUrl, buildResponseHeaders } from './lib/helpers.js';
+import { photobankClient } from './lib/photobankClient.js';
+import { parseUrl } from './lib/url.js';
+import { upstreamUrl, parseResizeOpts } from './lib/helpers.js';
+import { HTTP_INTERNAL_ERROR } from './lib/const.js';
 
 const { host: HOST, port: PORT } = config;
-const HTTP_INTERNAL_ERROR = 500;
-const HTTP_OK = 200;
-
-const errorLogger = (method, url) => (msg) =>
-  logger.child({ method }).child({ url }).error(msg);
 
 const server = http.createServer((request, response) => {
   const { method, url } = request;
   const { path, params } = parseUrl(url);
-  const imageUrl = upstreamUrl(path);
   const resizeOpts = parseResizeOpts(params);
+  const imageUrl = upstreamUrl(path);
 
-  const logError = errorLogger(method, url);
-  const pipelineErrHandler = (err) => void (err && logError(err.message));
+  const log = logger.child({ method }).child({ url });
+  const handlePipeError = (e) => void (e && log.error(e.message));
 
   const abort = (statusCode, statusMessage) => {
     if (!response.headersSent) response.writeHead(statusCode, statusMessage);
     response.end();
   };
 
-  const upstreamReq = http.get(imageUrl, (upstreamResponse) => {
-    const { statusCode, statusMessage } = upstreamResponse;
+  const sendImage = ({ imageStream, statusCode, statusMessage, headers }) => {
+    const resize = sharp().resize(resizeOpts);
+    resize.once('readable', () => {
+      response.writeHead(statusCode, statusMessage, headers);
+    });
+    resize.on('error', function (err) {
+      log.error(`Resize error: ${err.message}`);
+      abort(HTTP_INTERNAL_ERROR, 'Resize error');
+      this.destroy();
+    });
 
-    if (statusCode !== HTTP_OK) {
-      logError(`Upstream bad response: ${statusCode} ${statusMessage}`);
-      abort(statusCode, statusMessage);
+    pipeline(imageStream, resize, response, handlePipeError);
+  };
+
+  const upstreamReq = photobankClient(imageUrl, (err, result) => {
+    if (err) {
+      log.error(err.message);
+      abort(err.statusCode, err.statusMessage);
     } else {
-      const resize = sharp().resize(resizeOpts);
-
-      resize.once('readable', () => {
-        const headers = buildResponseHeaders(upstreamResponse);
-        response.writeHead(statusCode, statusMessage, headers);
-      });
-      resize.on('error', function (err) {
-        logError(`Resize error: ${err.message}`);
-        abort(HTTP_INTERNAL_ERROR, 'Resize error');
-        this.destroy();
-      });
-
-      pipeline(upstreamResponse, resize, response, pipelineErrHandler);
+      const { response: imageStream, ...rest } = result;
+      sendImage({ imageStream, ...rest });
     }
   });
 
-  upstreamReq.on('error', (err) => {
-    logError(`Upstream communication error: ${err.message}`);
-    abort(HTTP_INTERNAL_ERROR, 'Upstream communication error');
-  });
-
-  pipeline(request, upstreamReq, pipelineErrHandler);
+  pipeline(request, upstreamReq, handlePipeError);
 });
 
 server.listen(PORT, HOST, () => {
