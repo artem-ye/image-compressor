d061d9af76b54218e2f7b9f44af48d7983a6ae88
ay <shelmafona@gmail.com>
2024-10-08 11:42:22 +0300

    add cb param to buildUpstreamRequest

--- index.js
+++ index.js
@@ -10,56 +10,67 @@ import { HTTP_OK, HTTP_INTERNAL_ERROR } from './lib/const.js';
 
 const { host: HOST, port: PORT } = config;
 
-const buildUpstreamRequest = ({ response, path, params, log }) => {
-  const imageUrl = upstreamUrl(path);
+const photobankClient = (url, cb) => {
+  const req = http.get(url, (response) => {
+    const { statusCode, statusMessage } = response;
+
+    if (statusCode !== HTTP_OK) {
+      const message = `Upstream bad response: ${statusCode} ${statusMessage}`;
+      cb({ message, statusCode, statusMessage, error: null });
+    } else {
+      const headers = buildResponseHeaders(response);
+      cb(null, { response, statusCode, statusMessage, headers });
+    }
+  });
+
+  req.on('error', (error) => {
+    cb({
+      message: `Upstream communication error: ${error.message}`,
+      statusCode: HTTP_INTERNAL_ERROR,
+      statusMessage: 'Upstream communication error',
+      error,
+    });
+  });
+
+  return req;
+};
+
+const server = http.createServer((request, response) => {
+  const { method, url } = request;
+  const { path, params } = parseUrl(url);
   const resizeOpts = parseResizeOpts(params);
+
+  const log = logger.child({ method }).child({ url });
+  const handlePipelineError = (e) => void (e && log.error(e.message));
 
   const abort = (statusCode, statusMessage) => {
     if (!response.headersSent) response.writeHead(statusCode, statusMessage);
     response.end();
   };
 
-  const upstreamReq = http.get(imageUrl, (upstreamResponse) => {
-    const { statusCode, statusMessage } = upstreamResponse;
-
-    if (statusCode !== HTTP_OK) {
-      log.error(`Upstream bad response: ${statusCode} ${statusMessage}`);
-      abort(statusCode, statusMessage);
-    } else {
-      const resize = sharp().resize(resizeOpts);
-
-      resize.once('readable', () => {
-        const headers = buildResponseHeaders(upstreamResponse);
-        response.writeHead(statusCode, statusMessage, headers);
-      });
-      resize.on('error', function (err) {
-        log.error(`Resize error: ${err.message}`);
-        abort(HTTP_INTERNAL_ERROR, 'Resize error');
-        this.destroy();
-      });
-
-      const stub = (e) => void (e && log.error(e.message));
-      pipeline(upstreamResponse, resize, response, stub);
+  const handleUpstreamResponse = (err, result) => {
+    if (err) {
+      log.error(err.message);
+      return void abort(err.statusCode, err.statusMessage);
     }
-  });
 
-  upstreamReq.on('error', (err) => {
-    log.error(`Upstream communication error: ${err.message}`);
-    abort(HTTP_INTERNAL_ERROR, 'Upstream communication error');
-  });
+    const { response: image, statusCode, statusMessage, headers } = result;
+    const resize = sharp().resize(resizeOpts);
+    resize.once('readable', () => {
+      response.writeHead(statusCode, statusMessage, headers);
+    });
+    resize.on('error', function (err) {
+      log.error(`Resize error: ${err.message}`);
+      abort(HTTP_INTERNAL_ERROR, 'Resize error');
+      this.destroy();
+    });
 
-  return upstreamReq;
-};
+    pipeline(image, resize, response, handlePipelineError);
+  };
 
-const server = http.createServer((request, response) => {
-  const { method, url } = request;
-  const { path, params } = parseUrl(url);
-
-  const log = logger.child({ method }).child({ url });
-
-  const upstreamReq = buildUpstreamRequest({ response, path, params, log });
-  const stub = (err) => void (err && log.error(err.message));
-  pipeline(request, upstreamReq, stub);
+  const imageUrl = upstreamUrl(path);
+  const upstreamReq = photobankClient(imageUrl, handleUpstreamResponse);
+  pipeline(request, upstreamReq, handlePipelineError);
 });
 
 server.listen(PORT, HOST, () => {
