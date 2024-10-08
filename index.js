5969f3543b20ee1b4d89b2dbf63446b8b648f7ae
ay <shelmafona@gmail.com>
2024-10-08 15:11:30 +0300

    v1.5.0 (logger)

--- index.js
+++ index.js
@@ -1,64 +1,65 @@
 import http from 'node:http';
 import { pipeline } from 'node:stream';
-
 import sharp from 'sharp';
-import { config } from './lib/config.js';
-import { parseUrl, parseResizeOpts } from './lib/url.js';
 
-const { host: HOST, port: PORT, upstreamEndpoint: UPSTREAM_ENDPOINT } = config;
+import { config } from './lib/config.js';
+import { logger } from './lib/logger.js';
+import { parseUrl, parseResizeOpts } from './lib/url.js';
+import { upstreamUrl, buildResponseHeaders } from './lib/helpers.js';
+
+const { host: HOST, port: PORT } = config;
 const HTTP_INTERNAL_ERROR = 500;
 const HTTP_OK = 200;
 
-const upstreamUrl = (path) => `${UPSTREAM_ENDPOINT}${path}`;
-
-const buildResponseHeaders = ({ headers: requestHeaders }) => {
-  const headers = { 'transfer-encoding': 'chunked' };
-  const { 'content-type': contentType } = requestHeaders;
-  contentType && (headers['content-type'] = contentType);
-  return headers;
-};
-
-const stub = () => {};
+const errorLogger = (method, url) => (msg) =>
+  logger.child({ method }).child({ url }).error(msg);
 
 const server = http.createServer((request, response) => {
-  const { path, params } = parseUrl(request.url);
+  const { method, url } = request;
+  const { path, params } = parseUrl(url);
   const imageUrl = upstreamUrl(path);
   const resizeOpts = parseResizeOpts(params);
 
-  const abort = (code, message) => {
-    if (!response.headersSent) response.writeHead(code, message);
+  const logError = errorLogger(method, url);
+  const pipelineErrHandler = (err) => void (err && logError(err.message));
+
+  const abort = (statusCode, statusMessage) => {
+    if (!response.headersSent) response.writeHead(statusCode, statusMessage);
     response.end();
-  };
-
-  const replyImage = (imageStream, statusCode, statusMessage, headers) => {
-    const resize = sharp()
-      .resize(resizeOpts)
-      .on('error', () => abort(HTTP_INTERNAL_ERROR, 'Resize error'))
-      .once('readable', () =>
-        response.writeHead(statusCode, statusMessage, headers)
-      );
-
-    pipeline(imageStream, resize, response, stub);
   };
 
   const upstreamReq = http.get(imageUrl, (upstreamResponse) => {
     const { statusCode, statusMessage } = upstreamResponse;
 
     if (statusCode !== HTTP_OK) {
+      logError(`Upstream bad response: ${statusCode} ${statusMessage}`);
       abort(statusCode, statusMessage);
     } else {
-      const headers = buildResponseHeaders(upstreamResponse);
-      replyImage(upstreamResponse, statusCode, statusMessage, headers);
+      const resize = sharp().resize(resizeOpts);
+
+      resize.once('readable', () => {
+        const headers = buildResponseHeaders(upstreamResponse);
+        response.writeHead(statusCode, statusMessage, headers);
+      });
+      resize.on('error', function (err) {
+        logError(`Resize error: ${err.message}`);
+        abort(HTTP_INTERNAL_ERROR, 'Resize error');
+        this.destroy();
+      });
+
+      pipeline(upstreamResponse, resize, response, pipelineErrHandler);
     }
   });
 
-  upstreamReq.on('error', () => {
+  upstreamReq.on('error', (err) => {
+    logError(`Upstream communication error: ${err.message}`);
     abort(HTTP_INTERNAL_ERROR, 'Upstream communication error');
   });
 
-  pipeline(request, upstreamReq, stub);
+  pipeline(request, upstreamReq, pipelineErrHandler);
 });
 
 server.listen(PORT, HOST, () => {
-  console.log('Server started on', server.address());
+  const { address, port } = server.address();
+  logger.info(`Server started on ${address}:${port}`);
 });
